c6b003b6c3c0c9549a8629405e841b40d8b39b59
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-04-20 22:37:23 +0200

    Implemented user authentication an route protections with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
@@ -10,7 +11,10 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imaginify",
+  title: {
+    template: "%s | Aurora_Imaginify",
+    default: 'Aurora_Imaginify',
+  },
   description: "AI-powered Image Generator",
 };
 
@@ -20,8 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+    
   );
 }
