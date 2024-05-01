b9b92fd9137932b10cbbd1775b726098daa06a6a
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-05-02 00:22:20 +0200

    corrected react/no-unescaped-entitiesduring build process

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,7 +16,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className='home'>
         <h1 className='home-heading'>
-          Unleash Your Creative Vision with Aurora's Imaginify
+          Unleash Your Creative Vision with Aurora&apos;s Imaginify
         </h1>
         <ul className='flex-center w-full gap-20'>
           {navLinks.slice(1, 5).map((link) => (
